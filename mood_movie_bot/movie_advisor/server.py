c2822efa02bff5c10540074e254f2b326c0c116c
jaybeljay <88445888+jaybeljay@users.noreply.github.com>
2021-11-22 17:07:26 +0600

    Update server.py
    Changed polling to webhook

--- mood_movie_bot/movie_advisor/server.py
+++ mood_movie_bot/movie_advisor/server.py
@@ -1,20 +1,31 @@
 import logging
 import os
 
-from aiogram import Bot, Dispatcher, executor, types
-import aiohttp
+from aiogram import Bot, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.webhook import SendMessage
+from aiogram.utils.executor import start_webhook
 
 from .services import get_mood_names, get_movie_by_mood
 from .exceptions import MoodDoesNotExist
 
 
 API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
-API_URL = os.getenv('TELEGRAM_API_URL')
+HEROKU_TOKEN = os.getenv('HEROKU_TOKEN')
+
+WEBHOOK_HOST = 'https://mood-movie-bot.herokuapp.com/'
+WEBHOOK_PATH = HEROKU_TOKEN
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+WEBAPP_HOST = 'localhost'
+WEBAPP_PORT = 3001
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
 moods = get_mood_names()
 
@@ -24,7 +35,7 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply(
+    await message.answer(
         "Hi!\n"
         "I'm MoodMovieBot!\n"
         "Tell me what's your mood today and I will recommend you a nice movie to watch.\n"
@@ -49,11 +60,31 @@ async def recommend_movie(message: types.Message):
         command_mood = message.get_command()
         movie = get_movie_by_mood(command_mood)
     except MoodDoesNotExist:
-        await message.answer("Sorry, I can't help. Try another mood.")
+        await SendMessage(message.chat.id, "Sorry, I can't help. Try another mood.")
         return
 
-    await message.answer(movie)
+    await SendMessage(message.chat.id, movie)
+    
+    
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    
+    
+async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+    await bot.delete_webhook()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+    logging.warning('Bye!')
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
